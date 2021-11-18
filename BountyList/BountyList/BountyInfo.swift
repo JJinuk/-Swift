5c4bdf8df18ee14a3b20225c0a4fc83be439cb96
Jinuk <jitoor610@gmail.com>
2021-11-18 17:02:53 +0900

    [1118]Remove_ .git files

---
+++ BountyList/BountyList/BountyInfo.swift
@@ -0,0 +1,22 @@
+//
+//  BountyInfo.swift
+//  BountyList
+//
+//  Created by 최진욱 on 2021/10/04.
+//
+
+import UIKit
+
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
---
+++ BountyList/BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by 최진욱 on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
