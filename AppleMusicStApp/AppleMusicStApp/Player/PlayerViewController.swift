57d5cf7b468e520c95a07a5d64bb20a90f791be1
Jinuk <jitoor610@gmail.com>
2021-12-09 16:59:40 +0900

    [1209]AppleMusicStApp - 14

--- AppleMusicStApp/AppleMusicStApp/Player/PlayerViewController.swift
+++ AppleMusicStApp/AppleMusicStApp/Player/PlayerViewController.swift
@@ -33,6 +33,8 @@ class PlayerViewController: UIViewController {
         updatePlayButton()
         updateTime(time: CMTime.zero)
         // TODO: TimeObserver 구현
+        timeObserver = simplePlayer.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10), queue: DispatchQueue.main, using: { time in self.updateTime(time: time)
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,7 +46,8 @@ class PlayerViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         // TODO: 뷰나갈때 처리 > 심플플레이어
-        
+        simplePlayer.pause()
+        simplePlayer.replaceCurrentItem(with: nil)
     }
     
     @IBAction func beginDrag(_ sender: UISlider) {
@@ -57,6 +60,11 @@ class PlayerViewController: UIViewController {
     
     @IBAction func seek(_ sender: UISlider) {
         // TODO: 시킹 구현
+        guard let currentItem = simplePlayer.currentItem else { return }
+        let position = Double(sender.value) // 0.... 1
+        let seconds = position * currentItem.duration.seconds
+        let time = CMTime(seconds: seconds, preferredTimescale: 100)
+        simplePlayer.seek(to: time)
     }
     
     @IBAction func togglePlayButton(_ sender: UIButton) {
@@ -76,9 +84,7 @@ extension PlayerViewController {
         guard let track = simplePlayer.currentItem?.convertToTrack() else { return }
         thumbnailImageView.image = track.artwork
         titleLabel.text = track.title
-        artistLabel.text = track.artist
-        
-        
+        artistLabel.text = track.artist        
     }
     
     func updateTintColor() {
@@ -91,13 +97,13 @@ extension PlayerViewController {
         // currentTime label, totalduration label, slider
         
         // TODO: 시간정보 업데이트, 심플플레이어 이용해서 수정
-        currentTimeLabel.text = secondsToString(sec: 0.0)   // 3.1234 >> 00:03
-        totalDurationLabel.text = secondsToString(sec: 0.0)  // 39.2045  >> 00:39
+        currentTimeLabel.text = secondsToString(sec: simplePlayer.currentTime)   // 3.1234 >> 00:03
+        totalDurationLabel.text = secondsToString(sec: simplePlayer.totalDurationTime)  // 39.2045  >> 00:39
         
         if isSeeking == false {
             // 노래 들으면서 시킹하면, 자꾸 슬라이더가 업데이트 됨, 따라서 시킹아닐때마 슬라이더 업데이트하자
             // TODO: 슬라이더 정보 업데이트
-            
+            timeSlider.value = Float(simplePlayer.currentTime/simplePlayer.totalDurationTime)
         }
     }
     
@@ -108,7 +114,6 @@ extension PlayerViewController {
         let seconds = totalSeconds % 60
         return String(format: "%02d:%02d", min, seconds)
     }
-    
     func updatePlayButton() {
         // TODO: 플레이버튼 업데이트 UI작업 > 재생/멈춤
         if simplePlayer.isPlaying {
@@ -119,7 +124,6 @@ extension PlayerViewController {
             let configuration = UIImage.SymbolConfiguration(pointSize: 40)
             let image = UIImage(systemName: "play.fill", withConfiguration: configuration)
             playControlButton.setImage(image, for: .normal)
-            
         }
     }
 }
