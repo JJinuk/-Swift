23e38390f6772af5d00bdbfff0e8fed5e2e518eb
Jinuk <jitoor610@gmail.com>
2021-12-10 23:19:10 +0900

    [1210]AppleMusicStApp - 15

--- AppleMusicStApp/AppleMusicStApp/Style/DefaultStyle.swift
+++ AppleMusicStApp/AppleMusicStApp/Style/DefaultStyle.swift
@@ -11,8 +11,8 @@ import UIKit
 public enum DefaultStyle {
     public enum Colors {
         public static let tint: UIColor = {
-            if #available(iOS 13.0, *) {
-                return UIColor { traitCollction in
+            if #available(iOS 13.0, *) {                                // 다크모드가 13이상 지원하므로
+                return UIColor { traitCollction in                      // 컬러를 반환할 때 traitCollection을 통해 다크모드인지 아닌지 알 수 있음
                     if traitCollction.userInterfaceStyle == .dark {
                         return .white
                     } else {
